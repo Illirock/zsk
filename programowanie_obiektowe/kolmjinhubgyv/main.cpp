ef7fa5748a9fc8989b28b0827212b4d605c63c4e
PMendelewski <90185123+PMendelewski@users.noreply.github.com>
2021-09-17 07:54:34 +0200

    Add files via upload

---
+++ programowanie_obiektowe/kolmjinhubgyv/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class animal
+{
+    public:
+    string name;
+    string species;
+    unsigned short int mass;
+    unsigned short int height;
+    unsigned short int paw;
+    char gender;
+    bool  tail;
+    bool tame;
+    bool endangered;
+    bool agresive;
+    bool vertevrate;
+    bool fly;
+
+    void showBasics
+    void showALL
+
+};
+int main()
+{
+
+}
