54aff2f8afe5b4acd9bc4ade46f28ef23ce2c4d7
Młoda Leokadia <90185123+PMendelewski@users.noreply.github.com>
2021-10-27 07:52:36 +0200

    Add files via upload

---
+++ programowanie_obiektowe/Inicjalizacja Obiektów_1.c
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class Worker
+{
+public:
+    int id {8};
+    string name {"Young"};
+    string surname {"Leosia"};
+
+    Worker();
+    Worker(int pId, string pName, string pSurname);
+
+    void getData();
+};
+
+Worker::Worker()
+{
+    id=13;
+    name="Domyœlne Imiê";
+    surname="Domyœlne Nazwisko";
+}
+Worker::Worker(int pId, string pName, string pSurname)
+{
+    id=pId;
+    name=pName;
+    surname=pSurname;
+}
+
+void Worker::getData()
+{
+    cout<<"Id: "<<id<<"\nImiê: "<<name<<", nazwisko: "<<surname<<endl;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_CTYPE, "polish");
+
+    Worker nowak;
+    nowak.getData();
+
+    Worker nowak1 = Worker(10, "OKI", "MOOD");
+    nowak1.getData();
+
+    Worker nowak2 = Worker();
+    nowak2.getData();
+}
