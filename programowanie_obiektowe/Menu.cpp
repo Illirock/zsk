679a3ee177d3a7023ee9679dbf9dd80765ecb2e1
Młoda Leokadia <90185123+PMendelewski@users.noreply.github.com>
2022-05-17 08:15:26 +0200

    Add files via upload

---
+++ programowanie_obiektowe/Menu.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+void totolotek(){
+	int array[6], lottery[6], win=0;
+	srand(time(NULL));
+	cout<<"Podaj twoje liczby do kumulacji"<<endl;
+	for(int i=0; i<6; i++){
+		cin>>array[i];
+	}
+	cout<<"Liczby kumulacji: "<<endl;
+	for(int j=0; j<6; j++){
+		lottery[j] = 1+rand()%(49-1+1);
+		cout<<"\n"<<lottery[j]<<", ";
+	}
+	for(int k=0; k<6; k++){
+		if(lottery[k]==array[6]){
+			win++;	
+		}
+	}
+	cout<<"\n\nTrafiles "<<win<<" liczb"<<endl;
+}
+void kolor(){
+		string color;
+		cout<<"Jaki jest twoj ulubiony kolor?"<<endl;
+		cin>>color;
+		cout<<"Twoj ulubiony kolor to : "<<color<<endl;
+}
+
+int main()
+{
+	bool menu = true;
+	int x;
+	while(menu)
+	{
+		cout<<"\nMenu\n0.Wyjscie\n1.Totolotek\n2.Ulubiony kolor"<<endl;
+		cin>>x;
+		switch(x){
+			case 0:
+				menu = false;
+				break;
+			case 1:
+				totolotek();
+				break;
+			case 2:
+				kolor();
+				break;
+			default:
+				cout<<"Error: podaj poprawna liczbe!"<<endl;
+				break;
+		}
+	}
+	cout<<"\nDo widzenia :D"<<endl;
+}
+
